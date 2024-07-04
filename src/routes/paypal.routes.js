fbbf3401731cbf6efc5c6d3197224a088bfa8b25
Dobledroid <carlosexequiel360@gmail.com>
2024-07-03 21:14:01 -0600

    createOrder

--- src/routes/paypal.routes.js
+++ src/routes/paypal.routes.js
@@ -13,14 +13,14 @@ const router = Router();
 
 router.post("/paypal/create-order", createOrder);
 
-router.post("/paypal/create-order-membresia", createOrderMembresia);
-router.post("/paypal/create-order-membresia-actualizar", createOrderMembresiaActualizar);
+// router.post("/paypal/create-order-membresia", createOrderMembresia);
+// router.post("/paypal/create-order-membresia-actualizar", createOrderMembresiaActualizar);
 
-router.get("/capture-order", captureOrder);
+// router.get("/capture-order", captureOrder);
 
-router.get("/capture-order-membresia", captureOrderMembresia);
-router.get("/capture-order-membresia-actualizar", captureOrderMembresiaActualizar);
+// router.get("/capture-order-membresia", captureOrderMembresia);
+// router.get("/capture-order-membresia-actualizar", captureOrderMembresiaActualizar);
 
-router.get("/cancel-order", cancelPayment);
+// router.get("/cancel-order", cancelPayment);
 
 export default router;
