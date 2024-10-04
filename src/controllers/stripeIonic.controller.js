39c6d55f496426827f04a7c06d3625d7d71c796c
Dobledroid <carlosexequiel360@gmail.com>
2024-10-04 00:12:52 -0600

    modificación stripe, ahora recibe el monto

--- src/controllers/stripeIonic.controller.js
+++ src/controllers/stripeIonic.controller.js
@@ -1,23 +1,28 @@
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 // Crear Payment Intent
 export const createPaymentIntent = async (req, res) => {
-    try {
-      //CAMBIO VERCEL
-      // Valores ficticios para probar el pago
-      const amount = 1000; // Monto fijo (por ejemplo, 1000 centavos equivale a 10.00 MXN)
-      const currency = 'mxn'; // Moneda fija (MXN)
-  
-      // Crear un Payment Intent con la cantidad y la moneda proporcionadas
-      const paymentIntent = await stripe.paymentIntents.create({
-        amount, // El monto en la moneda mínima (por ejemplo, 1000 para 10.00 MXN)
-        currency, // Debe coincidir con la moneda usada en el frontend
-        payment_method_types: ['card'],
-      });
-  
-      // Enviar el client secret al frontend
-      res.status(200).send({ clientSecret: paymentIntent.client_secret });
-    } catch (error) {
-      console.error('Error al crear el Payment Intent:', error);
-      res.status(500).send({ error: error.message });
+  try {
+    // Extraer el monto del cuerpo de la solicitud
+    const { amount } = req.body;
+    
+    // Validar que el monto sea un número válido
+    if (!amount || typeof amount !== 'number') {
+      return res.status(400).send({ error: 'Monto inválido.' });
     }
-  };
+
+    const currency = 'mxn'; // Moneda fija (MXN)
+
+    // Crear un Payment Intent con la cantidad y la moneda proporcionadas
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount: Math.round(amount * 100), // Convertir a centavos
+      currency, // Debe coincidir con la moneda usada en el frontend
+      payment_method_types: ['card'],
+    });
+
+    // Enviar el client secret al frontend
+    res.status(200).send({ clientSecret: paymentIntent.client_secret });
+  } catch (error) {
+    console.error('Error al crear el Payment Intent:', error);
+    res.status(500).send({ error: error.message });
+  }
+};
