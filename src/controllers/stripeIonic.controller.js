b3cb05e06e19e157664b91c88fb8b86cd40a223f
Dobledroid <carlosexequiel360@gmail.com>
2024-10-04 01:23:54 -0600

    nueva modificación

--- src/controllers/stripeIonic.controller.js
+++ src/controllers/stripeIonic.controller.js
@@ -97,3 +97,74 @@ export const createPaymentIntent = async (req, res) => {
     res.status(500).send({ error: error.message });
   }
 };
+
+
+export const processPayment = async (req, res) => {
+  try {
+    console.log('Procesando el pago...');
+
+    const { paymentIntentId, ID_usuario, ID_direccion, amount } = req.body;
+    console.log("Datos recibidos ", paymentIntentId, ID_usuario, ID_direccion, amount);
+
+    // Obtener el estado del Payment Intent
+    console.log('Obteniendo el Payment Intent...');
+    const paymentIntent = await stripe.paymentIntents.retrieve(paymentIntentId);
+    console.log('Estado del Payment Intent:', paymentIntent.status);
+
+    if (paymentIntent && paymentIntent.status === 'succeeded') {
+      console.log('El Payment Intent ha sido completado exitosamente.');
+
+      const fechaHoraActual = new Date().toISOString();
+
+      // Crear una nueva orden de pedido
+      console.log('Creando nueva orden de pedido...');
+      const ID_pedido = await addNewOrdenPedido({
+        ID_usuario,
+        fecha: fechaHoraActual,
+        total: amount,
+        operacion_id: paymentIntent.id,
+        operacion_status: paymentIntent.status,
+        ID_direccion,
+      });
+      console.log('Orden de pedido creada con ID:', ID_pedido);
+
+      // Obtener los ítems del carrito del usuario
+      console.log('Obteniendo ítems del carrito del usuario...');
+      const items = await getItemsOrderByUserID(ID_usuario);
+      console.log('Ítems obtenidos:', items);
+
+      // Agregar detalles del pedido
+      for (const item of items) {
+        console.log(`Agregando detalle del pedido para el producto ${item.ID_producto}...`);
+        await addNewDetallePedido({
+          ID_pedido,
+          ID_producto: item.ID_producto,
+          cantidad: item.cantidad,
+          precioUnitario: item.precioFinal,
+        });
+        console.log(`Detalle del pedido agregado para el producto ${item.ID_producto}.`);
+
+        // Actualizar la cantidad del producto
+        console.log(`Actualizando la cantidad del producto ${item.ID_producto}...`);
+        await updateItemQuantityByID_Orden({
+          ID_producto: item.ID_producto,
+          cantidad: item.cantidad,
+        });
+        console.log(`Cantidad del producto ${item.ID_producto} actualizada.`);
+      }
+
+      // Eliminar los ítems del carrito del usuario
+      console.log('Eliminando ítems del carrito del usuario...');
+      await deleteItemsByUserID(ID_usuario);
+      console.log('Ítems del carrito eliminados.');
+
+      res.status(200).send({ success: true });
+    } else {
+      console.error('El pago no se completó correctamente. Estado del Payment Intent:', paymentIntent?.status);
+      res.status(400).json({ message: "El pago no se completó correctamente" });
+    }
+  } catch (error) {
+    console.error('Error al procesar el pago:', error);
+    res.status(500).send({ error: error.message });
+  }
+};
--- src/routes/stripeIonic.routes.js
+++ src/routes/stripeIonic.routes.js
@@ -1,11 +1,16 @@
 import { Router } from "express";
 import {
   createPaymentIntent,
+  processPayment
 } from "../controllers/stripeIonic.controller";
 
 const router = Router();
 
 
+
 router.post("/create-payment-intent-ionic", createPaymentIntent);
+
+
+router.post("/process-payment-ionic", processPayment);
 
 export default router;
